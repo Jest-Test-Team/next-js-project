25826d0f4d61efeb3037f1c714fa104bfa0d32f5
e.an <161005688+estateangela@users.noreply.github.com>
2025-08-29 17:36:52 +0800

    update

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,9 +1,8 @@
-// next.config.ts
 import type { Config } from "next";
 
 const config: Config = {
   // ... 其他設定
-  output: 'export', // <--- 加入這一行
+  // output: 'export', // <--- 請刪除或註解掉這一行！
 };
 
 export default config;
